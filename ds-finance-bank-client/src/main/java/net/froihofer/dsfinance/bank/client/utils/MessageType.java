328e6a8664503efc607851c3146d006ebd414c15
ElerWohlmuthFH <eler.wohlmuth@gmail.com>
2023-01-16 21:20:51 +0100

    added comments to MessageType.java

--- ds-finance-bank-client/src/main/java/net/froihofer/dsfinance/bank/client/utils/MessageType.java
+++ ds-finance-bank-client/src/main/java/net/froihofer/dsfinance/bank/client/utils/MessageType.java
@@ -5,20 +5,42 @@ package net.froihofer.dsfinance.bank.client.utils;
  * Declaration on Message Types in Console for different Response Messages used in UserInterface.java
  */
 public enum MessageType {
+    /**
+     * Enum value for a success message, represented by the color green.
+     */
     SUCCESS("\u001B[32m"),
+    /**
+     * Enum value for an error message, represented by the color red.
+     */
     ERROR("\u001B[31m"),
+    /**
+     * Enum value for an informational message, represented by the color blue.
+     */
     INFO("\u001B[34m"),
+    /**
+     * Enum value for a headline message, represented by the color cyan.
+     */
     HEADLINE("\u001B[36m"),
+    /**
+     * Enum value to reset the console color.
+     */
     RESET("\u001B[0m");
 
     private final String code;
 
+    /**
+     * Constructor for the MessageType enum
+     * @param code the string code associated with the message type
+     */
     MessageType(String code) {
         this.code = code;
     }
 
+    /**
+     * Getter for the code field
+     * @return the code associated with the message type
+     */
     public String getCode(){
         return this.code;
     }
-
 }
