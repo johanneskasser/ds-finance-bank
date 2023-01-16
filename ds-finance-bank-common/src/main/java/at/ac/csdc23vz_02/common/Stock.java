8ed20d7859baaab705bb030eb7e3bb80b4cb4336
Zeynep-Avci <zeynep.avci.66@hotmail.com>
2023-01-16 20:12:36 +0100

    Stock commented

--- ds-finance-bank-common/src/main/java/at/ac/csdc23vz_02/common/Stock.java
+++ ds-finance-bank-common/src/main/java/at/ac/csdc23vz_02/common/Stock.java
@@ -16,6 +16,11 @@ public class Stock implements Serializable {
     private String stockExchange;
     private String symbol;
 
+    /*
+    The constructor for the Stock class. It assigns the parameters to the corresponding fields
+    of the class using the 'this' keyword. The constructor is called when a new Stock object is created and
+    it is used to initialize the fields of the object with the provided values.
+     */
     public Stock(String companyName, Double lastTradePrice, Date lastTradeTime, Long marketCapitalization, String stockExchange, String symbol) {
         this.companyName = companyName;
         this.lastTradePrice = lastTradePrice;
@@ -28,54 +33,68 @@ public class Stock implements Serializable {
     public Stock() {
     }
 
+    //getter method of companyName. It retrieves the current value of the companyName field.
     public String getCompanyName() {
         return companyName;
     }
 
+    //setter method of companyName. Updates the value of the companyName field.
     public void setCompanyName(String companyName) {
         this.companyName = companyName;
     }
 
+    //getter method of lastTradePrice. It retrieves the current value of the lastTradePrice field.
     public Double getLastTradePrice() {
         return lastTradePrice;
     }
 
+    //setter method of lastTradePrice. Updates the value of the lastTradePrice field.
     public void setLastTradePrice(Double lastTradePrice) {
         this.lastTradePrice = lastTradePrice;
     }
 
+    //getter method of lastTradeTime. It retrieves the current value of the lastTradeTime field.
     public Date getLastTradeTime() {
         return lastTradeTime;
     }
 
+    //setter method of lastTradeTime. Updates the value of the lastTradeTime field.
     public void setLastTradeTime(Date lastTradeTime) {
         this.lastTradeTime = lastTradeTime;
     }
 
+    //getter method of marketCapitalization. It retrieves the current value of the marketCapitalization field.
     public Long getMarketCapitalization() {
         return marketCapitalization;
     }
 
+    //setter method of marketCapitalization. Updates the value of the marketCapitalization field.
     public void setMarketCapitalization(Long marketCapitalization) {
         this.marketCapitalization = marketCapitalization;
     }
 
+    //getter method of stockExchange. It retrieves the current value of the stockExchange field.
     public String getStockExchange() {
         return stockExchange;
     }
 
+    //setter method of stockExchange. Updates the value of the stockExchange field.
     public void setStockExchange(String stockExchange) {
         this.stockExchange = stockExchange;
     }
 
+    //getter method of symbol. It retrieves the current value of the symbol field.
     public String getSymbol() {
         return symbol;
     }
 
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
-    }
+    //setter method of symbol. Updates the value of the symbol field.
+    public void setSymbol(String symbol) { this.symbol = symbol; }
 
+    /*
+    With the getter methods from above, which get the values of the fields, the toString() method
+    returns a String. It displays the contents of the objects in a human-readable format.
+     */
     public String toString() {
         return "Company Name: " + this.getCompanyName() + "\n" +
                 "Symbol: " + this.getSymbol() + "\n" +
