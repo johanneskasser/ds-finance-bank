b4059042debc7f34203b6f4492675cd1d9910483
Zeynep-Avci <zeynep.avci.66@hotmail.com>
2023-01-16 21:14:21 +0100

    UserType.java and MessageType.java were commented

--- ds-finance-bank-client/src/main/java/net/froihofer/dsfinance/bank/client/utils/MessageType.java
+++ ds-finance-bank-client/src/main/java/net/froihofer/dsfinance/bank/client/utils/MessageType.java
@@ -4,6 +4,7 @@ package net.froihofer.dsfinance.bank.client.utils;
 /**
  * Declaration on Message Types in Console for different Response Messages used in UserInterface.java
  */
+//We have five MessageTypes as above. The code is used to change the color of text displayed in the console.
 public enum MessageType {
     SUCCESS("\u001B[32m"),
     ERROR("\u001B[31m"),
@@ -13,10 +14,13 @@ public enum MessageType {
 
     private final String code;
 
+    //The constructor is called when a new enum value is created and sets the initial state of the enum
+    // value by passing in the necessary values as arguments.
     MessageType(String code) {
         this.code = code;
     }
 
+    //return the code associated with the particular enum value.
     public String getCode(){
         return this.code;
     }
--- ds-finance-bank-client/src/main/java/net/froihofer/dsfinance/bank/client/utils/UserType.java
+++ ds-finance-bank-client/src/main/java/net/froihofer/dsfinance/bank/client/utils/UserType.java
@@ -1,5 +1,7 @@
 package net.froihofer.dsfinance.bank.client.utils;
 
+// we have two UserTypes, which are EMPLOYEE and CUSTOMER. We can set a variable to one of the two values and based on
+// the value of the variable, we can perform different actions.
 public enum UserType {
     EMPLOYEE, CUSTOMER
 }
