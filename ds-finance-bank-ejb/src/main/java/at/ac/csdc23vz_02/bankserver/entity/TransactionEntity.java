b95434d9be938b5c5225c091344d91ecb528e163
Mario Ikic <71633522+c2010475020@users.noreply.github.com>
2023-01-16 21:27:13 +0100

    TransactionsEntity comments completed

--- ds-finance-bank-ejb/src/main/java/at/ac/csdc23vz_02/bankserver/entity/TransactionEntity.java
+++ ds-finance-bank-ejb/src/main/java/at/ac/csdc23vz_02/bankserver/entity/TransactionEntity.java
@@ -97,61 +97,100 @@ public class TransactionEntity implements Serializable {
         this.tradeTime = stock.getLastTradeTime();
     }
     /**
-     * Method to get the price where the stock share is bought
-     * @return returns BuyPrice from the database
+     * Method to get BuyPrice of a TransactionEntity
+     * @return returns buyPrice from the database
      */
     public BigDecimal getBuyPrice() {
         return buyPrice;
     }
-
+    /**
+     * Method to set BuyPrice of a TransactionEntity
+     * @return returns buyPrice from the database
+     */
     public void setBuyPrice(BigDecimal buyPrice) {
         this.buyPrice = buyPrice;
     }
-
+    /**
+     * Method to get ID of a TransactionEntity
+     * @return returns ID from the database
+     */
     public Integer getID() {
         return ID;
     }
-
+    /**
+     * Method to set ID of a TransactionEntity
+     * @return returns ID from the database
+     */
     public void setID(Integer ID) {
         this.ID = ID;
     }
-
+    /**
+     * Method to get CostumerID of a TransactionEntity
+     * @return returns costumerID from the database
+     */
     public Integer getCustomerID() {
         return customerID;
     }
-
+    /**
+     * Method to set CostumerID of a TransactionEntity
+     * @return returns costumerID from the database
+     */
     public void setCustomerID(Integer customerID) {
         this.customerID = customerID;
     }
-
+    /**
+     * Method to get StockSymbol of a TransactionEntity
+     * @return returns stocksymbol from the database
+     */
     public String getStockSymbol() {
         return stockSymbol;
     }
-
+    /**
+     * Method to set StockSymbol of a TransactionEntity
+     * @return returns stockSymbol from the database
+     */
     public void setStockSymbol(String stockSymbol) {
         this.stockSymbol = stockSymbol;
     }
-
+    /**
+     * Method to get CompanyName of a TransactionEntity
+     * @return returns companyName from the database
+     */
     public String getCompanyName() {
         return companyName;
     }
-
+    /**
+     * Method to set CompanyName of a TransactionEntity
+     * @return returns companyName from the database
+     */
     public void setCompanyName(String companyName) {
         this.companyName = companyName;
     }
-
+    /**
+     * Method to get ShareCount of a TransactionEntity
+     * @return returns shareCount from the database
+     */
     public Integer getShareCount() {
         return shareCount;
     }
-
+    /**
+     * Method to set ShareCount of a TransactionEntity
+     * @return returns shareCount from the database
+     */
     public void setShareCount(Integer shareCount) {
         this.shareCount = shareCount;
     }
-
+    /**
+     * Method to get getTradeTime of a TransactionEntity
+     * @return returns tradeTime from the database
+     */
     public Date getTradeTime() {
         return tradeTime;
     }
-
+    /**
+     * Method to set TradeTime of a TransactionEntity
+     * @return returns tradeTime from the database
+     */
     public void setTradeTime(Date tradeTime) {
         this.tradeTime = tradeTime;
     }
